60c4d28c49b651aa908c2a46f408309d563ff127
Oskar Kobierecki/WAW/PL <oskar.kobierecki@avon.com>
2019-06-11 17:27:20 +0200

    countdown added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './resources/resources/styles.css';
 
 import Header from './components/header_footer/header';
+import Featured from './components/featured/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: '2000px', background: 'cornflowerblue'}}>
           <Header></Header>
+          <Featured></Featured>
       </div>
     );
   }
